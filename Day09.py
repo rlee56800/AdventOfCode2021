0c4a7cb2592b93832de0f1a8b040ba20955865db
rlee56800 <78502232+rlee56800@users.noreply.github.com>
2021-12-09 02:02:22 -0800

    9 added pt 2 (branch and bound???)

--- Day09.py
+++ Day09.py
@@ -89,3 +89,49 @@ def find_lowests():
 # call functions
 find_lowests()
 print(risk_level(lowest))
+
+'''
+--- Part Two ---
+
+Next, you need to find the largest basins so you know what areas are most important to avoid.
+
+A basin is all locations that eventually flow downward to a single low point. Therefore, every low point has a basin, although some basins are very small. Locations of height 9 do not count as being in any basin, and all other locations will always be part of exactly one basin.
+
+The size of a basin is the number of locations within the basin, including the low point. The example above has four basins.
+
+The top-left basin, size 3:
+
+*2**1*99943210
+*3*987894921
+9856789892
+8767896789
+9899965678
+
+The top-right basin, size 9:
+
+21999*4**3**2*1**0*
+398789*4*9*2**1*
+9856789892
+8767896789
+9899965678
+
+The middle basin, size 14:
+
+2199943210
+39*8**7**8*94921
+9*8**5**6**7**8*9892
+*8**7**6**7**8*96789
+9*8*99965678
+
+The bottom-right basin, size 9:
+
+2199943210
+3987894921
+9856789*8*92
+876789*6**7**8*9
+98999**6**5**6**7**8*
+
+Find the three largest basins and multiply their sizes together. In the above example, this is 9 * 14 * 9 = 1134.
+
+What do you get if you multiply together the sizes of the three largest basins?
+'''
